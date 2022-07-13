b2dbc2758e23e75435f28ad06fb088d4be767d3b
noceive <nocivescience@gmail.com>
2022-07-13 19:26:20 -0400

    terminado

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const btnDelete=document.querySelector('.btn-delete');
 const textareaQuestion1El=document.querySelector('#exampleFormControlTextarea1');
 const textareaAnswer2El=document.querySelector('#exampleFormControlTextarea2');
 const storageEl=document.querySelector('.storage');
+const stageEl=document.querySelector('.stage');
 // const btnInfo=document.querySelector('.btn-info');
 // const btnLight=document.querySelector('.btn-light');
 // const btnDark=document.querySelector('.btn-dark');
@@ -24,9 +25,14 @@ function createCard(data,index){
     const card=document.createElement('div');
     card.classList.add('card');
     card.innerHTML=`
-        <div>
-            ${data.question}
+    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+        <div class="card-header">tarjeta n°${index+1}</div>
+            <div class="card-body">
+              <h5 class="card-title">${data.question}</h5>
+              <p class="card-text">${data.answer}</p>
+            </div>
         </div>
+    </div>
     `;
     storageEl.appendChild(card);
 }
