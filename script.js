72ce25ad5b7249579378deb5c4eae49852e364fb
noceive <nocivescience@gmail.com>
2022-07-13 17:36:05 -0400

    ya se recibe en la pagina

--- script.js
+++ script.js
@@ -5,24 +5,42 @@ const btnEnter=document.querySelector('.btn-enter');
 const btnDelete=document.querySelector('.btn-delete');
 const textareaQuestion1El=document.querySelector('#exampleFormControlTextarea1');
 const textareaAnswer2El=document.querySelector('#exampleFormControlTextarea2');
+const storageEl=document.querySelector('.storage');
 // const btnInfo=document.querySelector('.btn-info');
 // const btnLight=document.querySelector('.btn-light');
 // const btnDark=document.querySelector('.btn-dark');
 let currentCard=0;
 const cardsEl=[];
+const cardsData=getCardData();
 function setCardData(card){
     localStorage.setItem('cards',JSON.stringify(card));
     window.location.reload();
 }
+function getCardData(){
+    const card=JSON.parse(localStorage.getItem('cards'));
+    return card===null?[]:card;
+}
 function createCard(data,index){
     const card=document.createElement('div');
     card.classList.add('card');
+    card.innerHTML=`
+        <div>
+            ${data.question}
+        </div>
+    `;
+    storageEl.appendChild(card);
+}
+function createCards(){
+    cardsData.forEach((data,index)=>{
+        createCard(data,index);
+    });
 }
 btnEnter.addEventListener('click',()=>{
     const question=textareaQuestion1El.value;
     const answer=textareaAnswer2El.value;
     const newCard={question,answer};
-    cardsEl.push(newCard);
-    setCardData(cardsEl);
+    cardsData.push(newCard);
+    setCardData(cardsData);
+    createCard(newCard,currentCard);
 });
-
+createCards();
